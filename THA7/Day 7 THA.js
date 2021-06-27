f18b1924613c34b8781a7a4ebd8ec35bf68c928a
ayasahmad63 <85630168+ayasahmad63@users.noreply.github.com>
2021-06-27 22:50:45 +0530

    Add files via upload

---
+++ THA7/Day 7 THA.js
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 7 THA</title>
+    <script>
+        //Ans 1
+        // var student={namme:"David Rayy",sclass:"VI",rollno:12};
+        // for(let item in student){
+        //     console.log(item,',');
+        // }
+
+
+        //Ans 2
+        //  var student={namme:"David Rayy",sclass:"VI",rollno:12};
+        //  console.log(student);
+        //  delete student.rollno;
+        //  console.log(student);
+
+
+        //Ans 3
+        //   var student={namme:"David Rayy",sclass:"VI",rollno:12};
+       
+        //   var objsize1=0;
+        //  for(let item in student){
+        //      if(student.hasOwnProperty(item))
+        //      objsize1++;
+        //  }
+        //     console.log('Object size is ='+objsize1);
+
+        //Ans 4
+        // var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true },
+        //  { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true },
+        //   { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
+        // //   console.log(library);
+        
+        //   for(var i=0;i<library.length;i++)
+        //   {
+        //       var book="'"+library[i].title+" by "+library[i].author;
+        //       if(library[i].readingStatus)
+        //       console.log("You are already readen"+book);
+        //       else
+        //       console.log("Not readen yet"+book);
+        //   }
+ 
+        
+        //Ans 5
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
