dc7159f236c0ec985ed1062e98b128c167da4b54
Ayas Ahmad <ayasahmad63@gmail.com>
2021-07-24 14:53:04 +0530

    Add files via upload

---
+++ DAY 16/card/src/card.jsx
@@ -0,0 +1,9 @@
+function card (){
+    return(
+    <div>
+        <h1> React Card</h1>
+        <p>Your main THA is to make meme cards</p>
+    </div>
+    );
+}
+export default card;
---
+++ DAY 16/card/src/photo.jsx
@@ -0,0 +1,7 @@
+function photo(){
+    return(
+    <img src="https://webdevtrick.com/wp-content/uploads/pure-css-card-980x514.jpg" alt="" srcset="" />
+
+    );
+}
+export default photo;
