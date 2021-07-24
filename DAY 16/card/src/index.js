bebfce1da22d5713b5e5a9c5c37f7c03ae4814be
Ayas Ahmad <ayasahmad63@gmail.com>
2021-07-24 14:44:34 +0530

    Create index.js

---
+++ DAY 16/card/src/index.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./card.jsx"
+import Photo from "./photo.jsx"
+import "./styke.css"
+
+ReactDOM.render(
+   <div className="main">
+  <Photo />
+  <Card />
+  </div>
+  ,
+  document.getElementById("root")
+
+
+)
