eeb18c63bfd07afabe6b3adec7de7ad792dbed65
Ayas Ahmad <ayasahmad63@gmail.com>
2021-08-01 11:46:48 +0530

    Create App.js

---
+++ Day 19/src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App(props) {
+  return (
+  
+    <div className="App">
+      <header className="App-header">
+    <h1> 4 counter component </h1>
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
