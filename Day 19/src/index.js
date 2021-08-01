931e73ae232a2544f1e925cbf7f5df55a5b5698a
Ayas Ahmad <ayasahmad63@gmail.com>
2021-08-01 11:46:25 +0530

    Create index.js

---
+++ Day 19/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import Button1 from './Components/Button1'
+import Button2 from './Components/Button2'
+
+ReactDOM.render(
+
+  <React.StrictMode>
+     <App  />
+   <Button1 />
+   <Button2 />
+   <Button2 />
+   <Button2 />
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
