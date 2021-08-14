1ee106cbf179090f019654a490a8b526024d0515
Ayas Ahmad <ayasahmad63@gmail.com>
2021-08-14 09:43:15 +0530

    Add files via upload

---
+++ Day 27/Todo List Using Redux/src/store.js
@@ -0,0 +1,8 @@
+import rootReducer from "./reducers";
+import { createStore,applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+
+const store=createStore(rootReducer,applyMiddleware(logger));
+
+export default store;
