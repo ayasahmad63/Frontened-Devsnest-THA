2b34e9ffc22ff6c960c3c45e9570953e33b2dd21
Ayas Ahmad <ayasahmad63@gmail.com>
2021-08-20 17:01:52 +0530

    Create index.js

---
+++ Day 29/Wheather App Using Redux/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers} from "redux";
+import placeDataReducer from "./placeDataReducer";
+import placeReducer from "./placeReducer";
+
+const rootReducer=combineReducers({
+    place:placeReducer,
+    placeData:placeDataReducer,
+});
+export default rootReducer;
